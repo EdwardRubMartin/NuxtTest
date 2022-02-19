ef6e20ee092e9aa8751f682cf455333d19612903
Eduardo Rubio <edwardrubmartin@gmail.com>
2022-02-18 19:16:49 -0700

    feat: nuxt-test-first-commit

---
+++ pages/about.vue
@@ -0,0 +1,15 @@
+<template>
+  <div><p class="bg-pink-300 text-pink-700">Hola chicos y chicas</p></div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'AboutPage',
+  mounted() {
+    // eslint-disable-next-line no-console
+    console.log(this.$route)
+  },
+})
+</script>
